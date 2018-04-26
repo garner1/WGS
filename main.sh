9af9d6fbd0056156f940e8b4711c75bd683b5f17
garner1 <silvano.garnerone@gmail.com>
2018-04-26 09:19:04 +0200

    minor

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+fastq=$1
+exp=$2
+datadir=~/Work/dataset/WGS/$exp
+refgen=~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa # full path to reference genome
+numbproc=32
+quality=30
+
+echo "Processing " $exp 
+mkdir -p $datadir
+echo "Trimming ..."
+/home/garner1/anaconda2/envs/mypython3/bin/cutadapt -j $numbproc -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o $datadir/$exp.trimmed.fastq.gz $fastq > $datadir/trim.log
+echo "Aligning ..."
+bwa mem -v 1 -t $numbproc $refgen $datadir/$exp.trimmed.fastq.gz | samtools view -h -Sb -q $quality - > $datadir/$exp.bam 
+samtools sort $datadir/$exp.bam  -o $datadir/$exp.sorted.bam
+mv $datadir/$exp.sorted.bam $datadir/$exp.bam 
+samtools index $datadir/$exp.bam 
+echo "Done!"
+echo
---
+++ themall.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+exp=MN1
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN2
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN3
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN4
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN6
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN7
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN8
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN9
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN11
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN18
+bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
