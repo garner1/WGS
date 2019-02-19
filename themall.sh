df9a3db8de328baf20860e8f7b1885398bd8c34b
garner1 <silvano.garnerone@gmail.com>
2019-02-19 10:36:24 +0100

    minor

--- themall.sh
+++ themall.sh
@@ -1,23 +1,155 @@
 #!/usr/bin/env bash
 
-exp=MN10
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN12
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN13
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN14
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN15
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN17
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN19
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN20
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
-exp=MN5
-bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+exp=MN48
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN51
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN52
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN53
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN54
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN55
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN56
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+exp=MN57
+bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+
+##################################################################
+# exp=MN16
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN22
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN23
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN24
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN25
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN26
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN27
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN28
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+# exp=MN29
+# bash main.sh /home/garner1/Work/dataset/WGS/MN106/FASTQ/$exp.fastq.gz $exp
+
+# exp=MN34
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN35
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN36
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN37
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN38
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN39
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN40 
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN41 
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN42
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+# exp=MN43
+# bash main.sh /home/garner1/Work/dataset/WGS/MN107/$exp.fastq.gz $exp
+
+# exp=MN32
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN33
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN44
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN45
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN46
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN47
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN49
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+# exp=MN50
+# bash main.sh /home/garner1/Work/dataset/WGS/MN108/FASTQ/$exp.fastq.gz $exp
+########################################################
+# exp=MN30
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN31
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN41B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+
+# exp=MN50B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN51B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN52B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN53B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN54B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN55B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN56B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+# exp=MN57B
+# bash main.sh /home/garner1/Work/dataset/WGS/FASTQ/$exp.fastq.gz $exp
+
+##################################################
+# exp=MN-XZ11
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ12
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ13
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ14
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ15
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ16
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ17
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ18
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ19
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ20
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ21
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ22
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ23
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ24
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+# exp=MN-XZ25
+# bash main.sh ~/Work/dataset/WGS/$exp.fastq.gz $exp
+
+#################################################
+# exp=MN10
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN12
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN13
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN14
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN15
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN17
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN19
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN20
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
+# exp=MN5
+# bash main.sh /media/bicroserver_2-seq/BICRO82/FASTQ/$exp/$exp.fastq.gz $exp
 ###############################################
 # exp=MN1
 # bash main.sh /media/bicroserver_2-seq/BICRO71/FASTQ/$exp/$exp.fastq.gz $exp
